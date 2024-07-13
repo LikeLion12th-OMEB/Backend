39b07578220338b79e835ae47037194e6a18de39
siwan9 <si.wan208@gmail.com>
2024-07-13 20:17:10 +0900

    [docs] spring 초기 파일 추가

---
+++ src/main/java/com/example/OMEB/OmebApplication.java
@@ -0,0 +1,13 @@
+package com.example.OMEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmebApplication.class, args);
+	}
+
+}
