fecb425c8f8ea1d97e5fba99748d836d41e5d80b
Kkang <aorl2313@naver.com>
2024-07-26 04:16:52 +0900

    [refactor] swagger 관련 자잘한 리팩터링

--- src/main/java/com/example/OMEB/domain/book/presentation/controller/BookMarkController.java
+++ src/main/java/com/example/OMEB/domain/book/presentation/controller/BookMarkController.java
@@ -6,6 +6,7 @@ import com.example.OMEB.domain.book.presentation.dto.response.BookTitleListRespo
 import com.example.OMEB.domain.profile.presentaion.dto.PresignedUrlResponse;
 import com.example.OMEB.global.aop.AssignUserId;
 import com.example.OMEB.global.base.dto.ResponseBody;
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -37,7 +38,7 @@ public class BookMarkController {
     })
     @PostMapping("/v1/bookmark/{bookId}")
     @AssignUserId
-    public ResponseEntity<ResponseBody<Void>> saveBookMark(Long userId , @PathVariable @Schema(description = "책 id",example = "1") Long bookId) {
+    public ResponseEntity<ResponseBody<Void>> saveBookMark(@Schema(hidden = true) Long userId , @PathVariable @Schema(description = "책 id",example = "1") Long bookId) {
         bookMarkService.saveBookMark(userId, bookId);
         return ResponseEntity.ok(createSuccessResponse());
     }
@@ -50,7 +51,7 @@ public class BookMarkController {
     })
     @GetMapping("/v1/bookmark")
     @AssignUserId
-    public ResponseEntity<ResponseBody<BookTitleListResponse>> getBookMark(Long userId) { // TODO : 페이징 필요한지 확인
+    public ResponseEntity<ResponseBody<BookTitleListResponse>> getBookMark(@Schema(hidden = true) Long userId) { // TODO : 페이징 필요한지 확인
         return ResponseEntity.ok(createSuccessResponse(bookMarkService.findUserBookMark(userId)));
     }
 
@@ -62,7 +63,7 @@ public class BookMarkController {
     })
     @DeleteMapping("/v1/bookmark/{bookId}")
     @AssignUserId
-    public ResponseEntity<ResponseBody<Void>> deleteBookMark(Long userId,
+    public ResponseEntity<ResponseBody<Void>> deleteBookMark(@Schema(hidden = true) Long userId,
                                                              @PathVariable @Schema(description = "책 id",example = "1") Long bookId) {
         bookMarkService.deleteBookMark(userId, bookId);
         return ResponseEntity.ok(createSuccessResponse());
--- src/main/java/com/example/OMEB/domain/profile/presentaion/dto/PresignedUrlResponse.java
+++ src/main/java/com/example/OMEB/domain/profile/presentaion/dto/PresignedUrlResponse.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(name = "PresignedUrlResponse", description = "Presigned Url 응답")
 public class PresignedUrlResponse {
     @Schema(description = "Presigned Url", example = "https://presignedurl-sample-bucket.s3.amazonaws.com/test.txt?AWSAccessKeyId=ASIATYFK6QMU57LCNAM3&Expires=1549289788&x-amz-security-token=FQoGoiO")
     private String url;
