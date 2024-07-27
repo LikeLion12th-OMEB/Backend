ff3b29ea6775cea5975f4d27c5f4ef9dd8933864
siwan9 <si.wan208@gmail.com>
2024-07-28 01:28:48 +0900

    [refactor] 오류 해결 및 회원가입 로직 수정

--- src/main/java/com/example/OMEB/global/config/RedisConfig.java
+++ src/main/java/com/example/OMEB/global/config/RedisConfig.java
@@ -8,10 +8,12 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @PropertySource("classpath:/security/application-db.yml")
+@EnableRedisRepositories
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String host;
