28c1ba4c76ac2a5b9f5ee8d79dc9b56d7609a4f4
siwan9 <si.wan208@gmail.com>
2024-07-28 21:32:16 +0900

    [refactor] Auth Api Swagger 적용

--- src/main/java/com/example/OMEB/domain/auth/presentation/dto/response/SignUpResponse.java
+++ src/main/java/com/example/OMEB/domain/auth/presentation/dto/response/SignUpResponse.java
@@ -1,15 +1,21 @@
 package com.example.OMEB.domain.auth.presentation.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@Schema(name = "SignUpResponse", description = "회원가입 결과 응답")
 public class SignUpResponse {
+    @Schema(description = "유저 DB Id", example = "1")
     private Long userId;
+    @Schema(description = "유저 닉네임", example = "멋쟁이사자")
     private String nickname;
+    @Schema(description = "Jwt 액세스 토큰", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
     private String accessToken;
+    @Schema(description = "Jwt 리프레시 토큰", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
     private String refreshToken;
 
     @Builder
