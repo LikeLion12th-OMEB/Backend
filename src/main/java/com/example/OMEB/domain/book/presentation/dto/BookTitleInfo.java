8b59c57bfa25d4da82fc5f55b405379a5e6853a2
Kkang <aorl2313@naver.com>
2024-07-31 22:18:05 +0900

    [refactor] : shcema 미적용 리팩터링

--- src/main/java/com/example/OMEB/domain/book/presentation/dto/BookTitleInfo.java
+++ src/main/java/com/example/OMEB/domain/book/presentation/dto/BookTitleInfo.java
@@ -2,6 +2,7 @@ package com.example.OMEB.domain.book.presentation.dto;
 
 import com.example.OMEB.domain.book.persistence.entity.Book;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,11 +10,17 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Schema(name = "BookTitleInfo", description = "책 제목 정보")
 public class BookTitleInfo {
+	@Schema(description = "책 id", example = "1")
 	private Long bookId;
+	@Schema(description = "책 제목", example = "책 제목")
 	private String title;
+	@Schema(description = "책 이미지 url", example = "http://image.com")
 	private String imageUrl;
+	@Schema(description = "책 가격", example = "10000")
 	private double price;
+	@Schema(description = "책 저자", example = "책 저자")
 	private String author;
 
 
