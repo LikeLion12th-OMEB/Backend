75b8bbe835d98caad58936a28d7bf20014418494
Kkang <aorl2313@naver.com>
2024-08-02 17:01:15 +0900

    [chore] : cors PATCH 메서드 추가

--- src/main/java/com/example/OMEB/global/config/SecurityConfig.java
+++ src/main/java/com/example/OMEB/global/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:3000");
         configuration.addAllowedOrigin("https://localhost:3000");
-        configuration.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT","PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
         //configuration.setMaxAge(3600L);
