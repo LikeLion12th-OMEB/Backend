0f2ba9f6c49ca0d25f9ca523e8283b9025489a88
Kkang <aorl2313@naver.com>
2024-08-04 14:30:10 +0900

    [chore] : review 관련 오류 수정

--- src/main/java/com/example/OMEB/domain/book/persistence/repository/TagRepository.java
+++ src/main/java/com/example/OMEB/domain/book/persistence/repository/TagRepository.java
@@ -1,6 +1,8 @@
 package com.example.OMEB.domain.book.persistence.repository;
 
 import com.example.OMEB.domain.review.persistence.entity.Tag;
+import com.example.OMEB.domain.review.persistence.vo.TagName;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import java.util.Optional;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag,Long> {
-    Optional<Tag> findByTagName(String tagName);
+    Optional<Tag> findByTagName(TagName tagName);
 }
--- src/main/java/com/example/OMEB/domain/review/application/service/ReviewService.java
+++ src/main/java/com/example/OMEB/domain/review/application/service/ReviewService.java
@@ -49,7 +49,7 @@ public class ReviewService {
                 .orElseThrow(() -> new ServiceException(ErrorCode.NOT_FOUND_USER));
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new ServiceException(ErrorCode.NOT_FOUND_BOOK));
-        Tag tag = tagRepository.findByTagName(reviewCreateRequest.getTag().toString())
+        Tag tag = tagRepository.findByTagName(reviewCreateRequest.getTag())
                 .orElseThrow(() -> new ServiceException(ErrorCode.NOT_FOUND_TAG));
         Review review = Review.fromReviewRequest(user, book, reviewCreateRequest,tag);
         reviewRepository.save(review);
@@ -80,8 +80,8 @@ public class ReviewService {
         }
         Tag preTag = review.getTag();
 
-        if(!preTag.toString().equals(reviewUpdateRequest.getTag())) {
-            Tag newTag = tagRepository.findByTagName(reviewUpdateRequest.getTag().toString())
+        if(!preTag.getTagName().toString().equals(reviewUpdateRequest.getTag().toString())) {
+            Tag newTag = tagRepository.findByTagName(reviewUpdateRequest.getTag())
                     .orElseThrow(() -> new ServiceException(ErrorCode.NOT_FOUND_TAG));
             preTag.getReviews().remove(review);
             preTag = newTag;
@@ -103,7 +103,7 @@ public class ReviewService {
 
     @Transactional(readOnly = true)
     public ReviewPageResponse getReviews(Long bookId, int page, int size, String sortDirection, String sortBy) {
-        Book book = bookRepository.findById(bookId)
+        bookRepository.findById(bookId)
                 .orElseThrow(() -> new ServiceException(ErrorCode.NOT_FOUND_BOOK));
         Pageable pageable = PageRequest.of(page, size, Sort.Direction.fromString(sortDirection), sortBy);
         return new ReviewPageResponse(reviewRepository.findAllByBookId(bookId, pageable));
@@ -135,7 +135,7 @@ public class ReviewService {
         if(!review.getUser().getId().equals(userId)) {
             throw new ServiceException(ErrorCode.REVIEW_NOT_MATCH_USER);
         }
-        User user = userRepository.findById(userId)
+        userRepository.findById(userId)
                 .orElseThrow(() -> new ServiceException(ErrorCode.NOT_FOUND_USER));
         review.getUser().getReviews().remove(review);
         reviewRepository.delete(review);
